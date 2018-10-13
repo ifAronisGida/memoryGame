b9af939e6bf8f654c26e11d2d8a774aacafb83a6
Aron Gida <ifAronisGida@github.com>
2018-10-14 00:11:55 +0200

    working arraygeneration for always winnable cards

--- static/game.js
+++ static/game.js
@@ -14,19 +14,28 @@ function turnDown(i) {
     cells[i].classList.replace(imagesArray[i], "fa-scroll");
 }
 
-// you can select elements by multiple ways, for example by their class
+function generateArray(cells) {
+    let firstHalf = ["fa-chess-knight", "fa-book-dead", "fa-dice-d6", "fa-dice-d20", "fa-dragon",
+        "fa-dungeon", "fa-fist-raised", "fa-hat-wizard", "fa-ring", "fa-skull-crossbones",
+        "fa-chess-knight", "fa-book-dead", "fa-dice-d6", "fa-dice-d20", "fa-dragon",
+        "fa-dungeon", "fa-fist-raised", "fa-hat-wizard", "fa-ring", "fa-skull-crossbones"];
+    let secondHalf = ["fa-chess-knight", "fa-book-dead", "fa-dice-d6", "fa-dice-d20", "fa-dragon",
+        "fa-dungeon", "fa-fist-raised", "fa-hat-wizard", "fa-ring", "fa-skull-crossbones",
+        "fa-chess-knight", "fa-book-dead", "fa-dice-d6", "fa-dice-d20", "fa-dragon",
+        "fa-dungeon", "fa-fist-raised", "fa-hat-wizard", "fa-ring", "fa-skull-crossbones"];
+    let imagesArray = [];
+    for (let i = 0; i < (cells.length) / 2; i++) {
+        imagesArray.push(firstHalf[i]);
+        imagesArray.push(secondHalf[i]);
+    }
+    return imagesArray;
+}
+
+
 let cells = document.getElementsByClassName('memCard');
-let imagesArray = ["fa-chess-knight", "fa-book-dead", "fa-dice-d6", "fa-dice-d20", "fa-dragon",
-    "fa-dungeon", "fa-fist-raised", "fa-hat-wizard", "fa-ring", "fa-skull-crossbones",
-    "fa-chess-knight", "fa-book-dead", "fa-dice-d6", "fa-dice-d20", "fa-dragon",
-    "fa-dungeon", "fa-fist-raised", "fa-hat-wizard", "fa-ring", "fa-skull-crossbones",
-    "fa-chess-knight", "fa-book-dead", "fa-dice-d6", "fa-dice-d20", "fa-dragon",
-    "fa-dungeon", "fa-fist-raised", "fa-hat-wizard", "fa-ring", "fa-skull-crossbones",
-    "fa-chess-knight", "fa-book-dead", "fa-dice-d6", "fa-dice-d20", "fa-dragon",
-    "fa-dungeon", "fa-fist-raised", "fa-hat-wizard", "fa-ring", "fa-skull-crossbones"];
+let imagesArray = shuffle(generateArray(cells));
 let click = 0;
 let prevCard;
-// shuffle(imagesArray);
 
 for (let i = 0; i < cells.length; i++) {
     // we uses classic for loop to have the "indexes" of the elements
@@ -38,8 +47,8 @@ for (let i = 0; i < cells.length; i++) {
         if (click % 2 === 1) {
             prevCard = i;
         }
-        if (click % 2 === 0) {
-            setTimeout(function(){ turnDown(i); turnDown(prevCard);}, 3000);
+        if (click % 2 === 0 && imagesArray[i] != imagesArray[prevCard]) {
+            setTimeout(function(){ turnDown(i); turnDown(prevCard);}, 750);
         }
     });
 }
